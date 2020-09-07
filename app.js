9e7c726f3670b9d866be6f2fa2e22c1ff9dbac22
Ridwan Balogun <balogunridwan@gmail.com>
2020-09-07 20:21:21 +0100

    Added tenor api and search functionality

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ const express = require("express");
 
 //App setup
 const app = express();
-
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "9GACLWOOOBL7", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_NG", // Your locale here, case-sensitivity depends on input
+});
 // Middleware
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Routes
-app.get("/", (req, res) => {
-    console.log(req.query)
-  res.render("home");
-});
+app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 app.get("/greetings/:name", (req, res) => {
   const name = req.params.name;
   res.render("greetings", { name });
