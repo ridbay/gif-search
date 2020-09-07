715dbc4fdb5ab8c54cb0b33aa9e41bed2ce8e4de
Ridwan Balogun <balogunridwan@gmail.com>
2020-09-07 18:47:19 +0100

    Added express-handlebars middleware

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const express = require("express");
 const app = express();
 
 // Middleware
-
+const exphbs = require("express-handlebars");
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
 // Routes
 app.get("/", (req, res)=>{
-    res.send("Hello Gif Search")
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    res.render('hello-gif', { gifUrl })
+})
+app.get("/greetings/:name", (req,res)=>{
+    const name = req.params.name;
+    res.render('greetings', {name})
 })
 // Start Server
 const PORT = 3000;
