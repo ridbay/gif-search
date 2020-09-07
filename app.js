f8f8cf7c30226cdf47a1635dbc372434888e3337
Ridwan Balogun <balogunridwan@gmail.com>
2020-09-07 19:03:42 +0100

    Added search form

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const app = express();
 
 // Middleware
 const exphbs = require("express-handlebars");
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // Routes
-app.get("/", (req, res)=>{
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    res.render('hello-gif', { gifUrl })
-})
-app.get("/greetings/:name", (req,res)=>{
-    const name = req.params.name;
-    res.render('greetings', {name})
-})
+app.get("/", (req, res) => {
+    console.log(req.query)
+  res.render("home");
+});
+app.get("/greetings/:name", (req, res) => {
+  const name = req.params.name;
+  res.render("greetings", { name });
+});
 // Start Server
 const PORT = 3000;
 app.listen(PORT, () => {
